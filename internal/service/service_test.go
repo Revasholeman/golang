adfaf8a55ca39445009cce2a6d9311b23b41c851
Sergey Pashkov <loccodev@mail.ru>
2024-07-28 18:57:59 +0200

    new test Run

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -47,14 +47,11 @@ func TestService_SpamMasker(t *testing.T) {
 		prod *mock.Mockproducer
 		pres *mock.Mockpresenter
 	}
-	type args struct {
-		message string
-	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   string
+		name    string
+		fields  fields
+		message string
+		want    string
 	}{
 		{
 			name: "TestService_SpamMasker",
@@ -62,10 +59,8 @@ func TestService_SpamMasker(t *testing.T) {
 				prod: mock.NewMockproducer(ctrl),
 				pres: mock.NewMockpresenter(ctrl),
 			},
-			args: args{
-				message: "http://123",
-			},
-			want: "http://***",
+			message: "http://123",
+			want:    "http://***",
 		},
 	}
 	for _, tt := range tests {
@@ -74,7 +69,7 @@ func TestService_SpamMasker(t *testing.T) {
 				prod: tt.fields.prod,
 				pres: tt.fields.pres,
 			}
-			if got := s.SpamMasker(tt.args.message); got != tt.want {
+			if got := s.SpamMasker(tt.message); got != tt.want {
 				t.Errorf("SpamMasker() = %v, want %v", got, tt.want)
 			}
 		})
@@ -85,25 +80,46 @@ func TestService_Run(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	mockProducer := mock.NewMockproducer(ctrl)
-	mockPresenter := mock.NewMockpresenter(ctrl)
-
-	expectedFile := []string{"input.txt", "output.txt"}
-	mockProducer.EXPECT().
-		Produce().
-		Return(expectedFile, nil)
-
-	mockPresenter.EXPECT().
-		Present(gomock.Any()).
-		Return(nil)
-
-	svc := &Service{
-		prod: mockProducer,
-		pres: mockPresenter,
+	type fields struct {
+		prod *mock.Mockproducer
+		pres *mock.Mockpresenter
+	}
+	tests := []struct {
+		name             string
+		fields           fields
+		expectedFiles    []string
+		expectedRunError error
+	}{
+		{
+			name: "Successful Run",
+			fields: fields{
+				prod: mock.NewMockproducer(ctrl),
+				pres: mock.NewMockpresenter(ctrl),
+			},
+			expectedFiles:    []string{"input.txt", "output.txt"},
+			expectedRunError: nil,
+		},
 	}
 
-	err := svc.Run()
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.fields.prod.EXPECT().
+				Produce().
+				Return(tt.expectedFiles, nil)
+
+			tt.fields.pres.EXPECT().
+				Present(gomock.Any()).
+				Return(nil)
+
+			s := &Service{
+				prod: tt.fields.prod,
+				pres: tt.fields.pres,
+			}
+
+			err := s.Run()
+			if err != tt.expectedRunError {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+		})
 	}
 }
